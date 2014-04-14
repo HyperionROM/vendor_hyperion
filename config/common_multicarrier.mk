SlimRoms/vendor_slim

Vendor: add multicarrier for unifyed devices
thanks CM

Change-Id: Ib2c84a21f9fbb6dc24edae2409dd0e8408619392

config/common_multicarrier.mk
@@ -1,0 +1,1 @@
+PRODUCT_PACKAGE_OVERLAYS += vendor/slim/overlay/multicarrier

HyperionROM/vendor_hyperion

config/common_multicarrier.mk
@@ -1,0 +1,1 @@
+PRODUCT_PACKAGE_OVERLAYS += vendor/hyperion/overlay/multicarrier